849f08b529c56be74e59dfb90767df024ad9d536
Badara <sopenabi8@gmail.com>
2022-06-17 12:28:49 +0000

    config postgressql

--- pointing/prod.py
+++ pointing/prod.py
@@ -1,5 +1,5 @@
 from pointing.settings import *
-
+import dj_database_url
 
 
 
@@ -12,3 +12,8 @@ TEMPLATE_DEBUG = False
 SECRET_KEY = 'DNt;*d7X4"MZ#;#%;YJEiApKs/[(TJ4l#>XYv$2G@n>7AkoROLYLXW)^=CaQ;a]'
 
 ALLOWED_HOSTS = ['qr-pointing.herokuapp.com']
+
+
+ 
+prod_db  =  dj_database_url.config()
+DATABASES['default'].update(prod_db)
