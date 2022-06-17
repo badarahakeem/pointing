c9f2d227fa2d8e1e538b91761ce11cd5a6808a70
Badara <sopenabi8@gmail.com>
2022-06-17 12:22:30 +0000

    404

---
+++ pointing/prod.py
@@ -0,0 +1,14 @@
+from pointing.settings import *
+
+
+
+
+
+
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+SECRET_KEY = 'DNt;*d7X4"MZ#;#%;YJEiApKs/[(TJ4l#>XYv$2G@n>7AkoROLYLXW)^=CaQ;a]'
+
+ALLOWED_HOSTS = ['qr-pointing.herokuapp.com']
