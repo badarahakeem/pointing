811c3d6aae95af1c0ce9441d88e555df85082815
Badara <sopenabi8@gmail.com>
2022-06-17 12:42:24 +0000

    config whitenoise

--- pointing/prod.py
+++ pointing/prod.py
@@ -13,7 +13,9 @@ SECRET_KEY = 'DNt;*d7X4"MZ#;#%;YJEiApKs/[(TJ4l#>XYv$2G@n>7AkoROLYLXW)^=CaQ;a]'
 
 ALLOWED_HOSTS = ['qr-pointing.herokuapp.com']
 
-
+MIDDLEWARE += ["whitenoise.middleware.WhiteNoiseMiddleware",]
  
 prod_db  =  dj_database_url.config()
 DATABASES['default'].update(prod_db)
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
