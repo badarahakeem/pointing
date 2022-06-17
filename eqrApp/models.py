a2b7cdbb4a8d681d65c32c77eaf6fb5a3ad0e2ba
Badara <sopenabi8@gmail.com>
2022-06-17 17:41:29 +0000

    cloudinary-js

--- eqrApp/models.py
+++ eqrApp/models.py
@@ -43,15 +43,15 @@ class Employee(models.Model):
             password = ''.join(secrets.choice(alphabet) for i in range(8))
             self.employee_code  = password
 
-        # qrcode_img = qrcode.make(self.employee_code)
-        # canvas = Image.new('RGB', (290,290), 'white')
-        # # draw = ImageDraw.Draw(canvas)
-        # canvas.paste(qrcode_img)
-        # fname = f'qr_code-{self.employee_code}'+'.png'
-        # buffer = BytesIO()
-        # canvas.save(buffer, 'PNG')
-        # self.qr_code.save(fname, File(buffer), save=False)
-        # canvas.close()
+        qrcode_img = qrcode.make(self.employee_code)
+        canvas = Image.new('RGB', (290,290), 'white')
+        # draw = ImageDraw.Draw(canvas)
+        canvas.paste(qrcode_img)
+        fname = f'qr_code-{self.employee_code}'+'.png'
+        buffer = BytesIO()
+        canvas.save(buffer, 'PNG')
+        self.qr_code.save(fname, File(buffer), save=False)
+        canvas.close()
         super().save(*args, **kwargs)
         # imag = Image.open(self.avatar.path)
         # if imag.width > 200 or imag.height > 200:
